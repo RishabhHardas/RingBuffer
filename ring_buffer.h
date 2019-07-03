40af056d8702d3bc22a2619bace7ff64b6678d8f
Rishabh Hardas <rishabhhardas@gmail.com>
2019-07-03 10:34:27 +0530

    Added struct member and function

--- ring_buffer.h
+++ ring_buffer.h
@@ -5,23 +5,32 @@
 #include <string.h>
 #include <stdio.h>
 
-#define BUFFER_LENGTH	256
+//Overflow handling
+#define OVERFLOW_IGNORE_LATEST		1
+#define OVERFLOW_OVERWRITE_OLDEST	2
+
+
+//#define HANDLE_OVERFLOW	OVERFLOW_IGNORE_LATEST
+
+#define BUFFER_LENGTH	7
 
 typedef struct {
 	char buffer[BUFFER_LENGTH];
 	int head;
 	int tail;
 	int count;
+	uint8_t overflow_handler;
 }ring_buffer;
 
 
-void ring_buffer_init(ring_buffer *rbuff);
+void ring_buffer_init(ring_buffer *rbuff, uint8_t overflow_handler);
 uint8_t ring_buffer_empty(ring_buffer *rbuff);
 uint8_t ring_buffer_full(ring_buffer *rbuff);
 int ring_buffer_get(ring_buffer *rbuff);
 void ring_buffer_put(ring_buffer *rbuff, const unsigned char ch);
 uint8_t ring_buffer_flush(ring_buffer *rbuf, uint8_t clear_buffer);
 void ring_buffer_print(ring_buffer *rbuff);
+void ring_buffer_change_overflow(ring_buffer *rbuff, uint8_t overflow_handler);
 
 
 #endif
