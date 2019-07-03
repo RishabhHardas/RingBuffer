b1a9235ff6bb511ca158890e0602eae691a8c1ea
Rishabh Hardas <rishabhhardas@gmail.com>
2019-07-03 10:33:11 +0530

    added function to change overflow method

--- ring_buffer.c
+++ ring_buffer.c
@@ -2,12 +2,16 @@
 
 
 
-void ring_buffer_init(ring_buffer *rbuff)
+void ring_buffer_init(ring_buffer *rbuff, uint8_t overflow_handler)
 {
 	rbuff->head = 0;
 	rbuff->count = 0;
 	rbuff->tail = 0;
-	
+
+	if (overflow_handler == OVERFLOW_IGNORE_LATEST ||
+	    overflow_handler == OVERFLOW_OVERWRITE_OLDEST)
+		rbuff->overflow_handler = overflow_handler;
+
 	//Can use library functions
 	int i = 0;
 	for (i = 0; i < BUFFER_LENGTH; i++) {
@@ -29,10 +33,10 @@ uint8_t ring_buffer_full(ring_buffer *rbuff)
 int ring_buffer_get(ring_buffer *rbuff)
 {
 	int temp;
-	
+
 	if (rbuff->count > 0) {
 		temp = rbuff->buffer[rbuff->tail];
-		
+
 		rbuff->tail = rbuff->tail+1;
 		if (rbuff->tail >= BUFFER_LENGTH) {
 			rbuff->tail = 0;
@@ -48,16 +52,31 @@ int ring_buffer_get(ring_buffer *rbuff)
 
 void ring_buffer_put(ring_buffer *rbuff, const unsigned char ch)
 {
-	if (rbuff->count < BUFFER_LENGTH) {
-		rbuff->buffer[rbuff->head] = ch;
-		
-		rbuff->head = rbuff->head+1;
-		if (rbuff->head >= BUFFER_LENGTH) {
-			rbuff->head = 0;
+	if (rbuff->overflow_handler == OVERFLOW_IGNORE_LATEST) {
+		if (rbuff->count < BUFFER_LENGTH) {
+			rbuff->buffer[rbuff->head] = ch;
+			rbuff->head = rbuff->head+1;
 		}
+	}
+	else {
+		rbuff->buffer[rbuff->head] = ch;
+		rbuff->head = rbuff->head+1;
+	}
 
+	if (rbuff->head >= BUFFER_LENGTH)
+		rbuff->head = 0;
+
+	if (rbuff->overflow_handler == OVERFLOW_IGNORE_LATEST) {
 		rbuff->count++;
 	}
+	else {
+		if (rbuff->count < BUFFER_LENGTH) {
+			rbuff->count = rbuff->count+1;
+		}
+		else
+			rbuff->count = rbuff->count;
+	}
+
 }
 
 uint8_t ring_buffer_flush(ring_buffer *rbuff, uint8_t clear_buffer)
@@ -81,4 +100,12 @@ void ring_buffer_print(ring_buffer *rbuff)
 			printf("%c", rbuff->buffer[itr]);
 		}
 	}
+}
+
+
+void ring_buffer_change_overflow(ring_buffer *rbuff, uint8_t overflow_handler)
+{
+	if (overflow_handler == OVERFLOW_IGNORE_LATEST ||
+	    overflow_handler == OVERFLOW_OVERWRITE_OLDEST)
+		rbuff->overflow_handler = overflow_handler;
 }
